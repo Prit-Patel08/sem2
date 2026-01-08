774bc895f2c493b4f314f0071eba9bb93b3bb94b
Prit-Patel08 <pritpatel102008@gmail.com>
2026-01-08 15:29:01 +0530

    Implement basic calculator functions in 25BCP022_calc.py

---
+++ 25BCP022_calc.py
@@ -0,0 +1,26 @@
+# Add two numbers
+
+def add():
+    a = int(input("Enter a number:"))
+    b = int(input("Enter Another Number:"))
+    print(a+b)
+
+def sub():
+    a = int(input("Enter a number:"))
+    b = int(input("Enter Another Number:"))
+    print(a-b)
+
+def mul():
+    a = int(input("Enter a number:"))
+    b = int(input("Enter Another Number:"))
+    print(a*b)
+
+def div():
+    a = int(input("Enter a number:"))
+    b = int(input("Enter Another Number:"))
+    print(a/b)
+
+add()
+sub()
+mul()
+div()
