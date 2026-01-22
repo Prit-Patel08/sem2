8d1a6d24de6fde8ce7ffed0c9b45bb5337d677a0
Prit-Patel08 <pritpatel102008@gmail.com>
2026-01-22 14:30:20 +0530

    Add files via upload

---
+++ smallest2.py
@@ -0,0 +1,10 @@
+def small(a, b):
+    if a < b:
+        print("Smallest:", a)
+    else:
+        print("Smallest:", b)
+
+x = int(input("Enter 1st number: "))
+y = int(input("Enter 2nd number: "))
+
+small(x, y)
