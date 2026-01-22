316ca34dd769db989fa0eef154865bc2952ef1ef
Prit-Patel08 <pritpatel102008@gmail.com>
2026-01-22 14:28:04 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,10 @@
+def large(a, b):
+    if a > b:
+        print("Largest:", a)
+    else:
+        print("Largest:", b)
+
+x = int(input("Enter 1st number: "))
+y = int(input("Enter 2nd number: "))
+
+large(x, y)
