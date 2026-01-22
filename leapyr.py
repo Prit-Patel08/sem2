9ee95acf63b3b78c46b2643712fc7f38b65402b0
Prit-Patel08 <pritpatel102008@gmail.com>
2026-01-22 14:51:18 +0530

    Create leapyr.py

---
+++ leapyr.py
@@ -0,0 +1,10 @@
+def leapyear():
+
+    year = int(input("Year: "))
+
+    if(year%4 == 0 and year%100 != 0 or year%400 == 0):
+        print("Leap year")
+    else:
+        print("Not a leap year")
+
+leapyear()
